d584a1f960719c4f1b66af8586165641f006183d
jbxamora <jbxamora@icloud.com>
2023-03-18 01:16:32 -0700

    added comments to client.js created route logic for general

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -6,8 +6,10 @@ import getCountryIso3 from "country-iso-2-to-3";
 
 export const getProducts = async (req, res) => {
     try {
+        // Get all products
         const products = await Product.find();
 
+        // Get product stats for each product and merge it with the product document
         const productsWithStats = await Promise.all(
             products.map(async (product) => {
                 const stat = await ProductStat.find({
@@ -20,6 +22,7 @@ export const getProducts = async (req, res) => {
             })
         );
 
+        // Return the list of products with their stats
         res.status(200).json(productsWithStats);
     } catch (error) {
         res.status(404).json({ message: error.message });
@@ -28,6 +31,7 @@ export const getProducts = async (req, res) => {
 
 export const getCustomers = async (req, res) => {
     try {
+        // Find all users with role "user" and exclude their password field
         const customers = await User.find({ role: "user" }).select("-password");
         res.status(200).json(customers);
     } catch (error) {
@@ -37,10 +41,10 @@ export const getCustomers = async (req, res) => {
 
 export const getTransactions = async (req, res) => {
     try {
-        // sort should look like this: { "field": "userId", "sort": "desc"}
+        // Get query parameters
         const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
 
-        // formatted sort should look like { userId: -1 }
+        // Generate sort object from query parameter
         const generateSort = () => {
             const sortParsed = JSON.parse(sort);
             const sortFormatted = {
@@ -51,6 +55,7 @@ export const getTransactions = async (req, res) => {
         };
         const sortFormatted = Boolean(sort) ? generateSort() : {};
 
+        // Find transactions matching the search query, sort them, and paginate the results
         const transactions = await Transaction.find({
             $or: [
                 { cost: { $regex: new RegExp(search, "i") } },
@@ -61,10 +66,12 @@ export const getTransactions = async (req, res) => {
             .skip(page * pageSize)
             .limit(pageSize);
 
+        // Get the total number of transactions matching the search query
         const total = await Transaction.countDocuments({
             name: { $regex: search, $options: "i" },
         });
 
+        // Return the list of transactions and the total count
         res.status(200).json({
             transactions,
             total,
@@ -76,8 +83,10 @@ export const getTransactions = async (req, res) => {
 
 export const getGeography = async (req, res) => {
     try {
+        // Find all users
         const users = await User.find();
 
+        // Map the locations to their ISO3 country code and count the occurrences of each country
         const mappedLocations = users.reduce((acc, { country }) => {
             const countryISO3 = getCountryIso3(country);
             if (!acc[countryISO3]) {
@@ -87,12 +96,14 @@ export const getGeography = async (req, res) => {
             return acc;
         }, {});
 
+        // Format the data for the world map visualization
         const formattedLocations = Object.entries(mappedLocations).map(
             ([country, count]) => {
                 return { id: country, value: count };
             }
         );
 
+        // Return the list of formatted locations
         res.status(200).json(formattedLocations);
     } catch (error) {
         res.status(404).json({ message: error.message });
